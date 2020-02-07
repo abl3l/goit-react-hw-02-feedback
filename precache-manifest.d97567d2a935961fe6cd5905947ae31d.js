e71b17201a1465b158b4b0a73ca9bba6ac68c72f
abl3l <7ckngMad.mn@gmail.com>
2020-02-07 14:08:38 +0200

    Updates

---
+++ precache-manifest.d97567d2a935961fe6cd5905947ae31d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca27140fc987909de9ac8851c4b791c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4088f5bc70a9cf7807a7",
+    "url": "/static/js/2.e2fcffeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e2fcffeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1dc3021782963905cf1b",
+    "url": "/static/js/main.03e7ea41.chunk.js"
+  },
+  {
+    "revision": "0499ecad0a8ab3fb4c2c",
+    "url": "/static/js/runtime-main.77c854ba.js"
+  }
+]);
